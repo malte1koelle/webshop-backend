1673e28284297a0bcbcb668d742976ea424d13ba
Malte Kölle <malte.koelle@unic.com>
2020-03-30 09:25:41 +0200

    Remove Requestmapping

--- src/main/java/com/bbw/webshopbackend/controller/ProductController.java
+++ src/main/java/com/bbw/webshopbackend/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.bbw.webshopbackend.model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.*;
@@ -13,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller // This means that this class is a Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class ProductController {
 
     @Autowired // This means to get the bean called userRepository
