189a67052e721240ac19f80f8555354e8d94b11b
Malte Kölle <malte.koelle@unic.com>
2020-03-30 09:23:53 +0200

    Add function getAllProducts

--- src/main/java/com/bbw/webshopbackend/WebshopBackendApplication.java
+++ src/main/java/com/bbw/webshopbackend/WebshopBackendApplication.java
@@ -1,13 +1,34 @@
 package com.bbw.webshopbackend;
 
+import com.bbw.webshopbackend.model.User;
+import com.bbw.webshopbackend.model.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class WebshopBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebshopBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(UserRepository repository) {
+		return (args) -> {
+// save a couple of customers
+			repository.save(new User());
+			repository.save(new User());
+			repository.save(new User());
+			repository.save(new User());
+			repository.save(new User());
+// fetch all customers
+
+		};
 	}
 
 }
